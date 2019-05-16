4594370b281df3bae05c98c081c069c548c44ffc
Amira Yah <al1709161@upy.edu.mx>
2019-05-16 14:09:21 -0500

    Second commit

--- mongodb.js
+++ mongodb.js
@@ -1,1 +1,15 @@
 Manual
+#Class 1
+#Class 2
+%Mystical Modifications%
+
+%%the $unseet operator can be used to remove specified fields
+
+db.potions.update(
+	{},
+	{"$unset": {"color": ""}}, %the value we pass doesn't impact the operation
+	{"multi": true} %%%Update all potions
+)
+
+#Clase 3
+%
